b441a593d1b2a8cb51ad9810194759716b8c721c
Elisha Peterson <elisha.peterson@jhuapl.edu>
2023-07-07 16:54:59 -0400

    copyrights

--- promptfx/src/main/kotlin/tri/promptfx/fun/ColorView.kt
+++ promptfx/src/main/kotlin/tri/promptfx/fun/ColorView.kt
@@ -23,7 +23,27 @@ class ColorView : AiPlanTaskView("Colors", "Enter a description of a color or ob
             val color = Color.web(result.trim())
             val fgColor = if (color.brightness > 0.5) "#000000" else "#ffffff"
             // update outputEditor foreground and background colors based on this
-            outputEditor.lookup(".content").style = "-fx-background-color: ${color.hex()};"
+            outputEditor.lookup(".content").style = "-fx-background-color: ${color.hex()};
+/*-
+ * #%L
+ * promptfx-0.1.0-SNAPSHOT
+ * %%
+ * Copyright (C) 2023 Johns Hopkins University Applied Physics Laboratory
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+"
             outputEditor.style = "-fx-text-fill: $fgColor;"
         } catch (x: IllegalArgumentException) {
             println("Failed to switch color. Result was $result.")
