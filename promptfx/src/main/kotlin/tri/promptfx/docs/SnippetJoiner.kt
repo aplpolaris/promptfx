fa2bf92708ec64e1b079422085d4590ad22d3bdd
Elisha Peterson <triathematician@gmail.com>
2024-04-28 21:16:40 -0400

    #203 document q&a preamble support

--- promptfx/src/main/kotlin/tri/promptfx/docs/SnippetJoiner.kt
+++ promptfx/src/main/kotlin/tri/promptfx/docs/SnippetJoiner.kt
@@ -21,6 +21,12 @@ package tri.promptfx.docs
 
 import tri.ai.embedding.EmbeddingMatch
 import tri.ai.prompt.AiPromptLibrary
+import tri.ai.text.chunks.TextDoc
+
+/** Name used in snippet joiner template for matching text. */
+const val MATCHES_TEMPLATE = "matches"
+/** Key used in [TextDoc] attributes for contextual information that will be added to any chunk information returned by the joiner. */
+const val TEXT_DOC_ATTRIBUTE_CONTEXT_PREFIX = "context-prefix"
 
 /**
  * Strategy for constructing a context from a set of matches.
@@ -31,24 +37,29 @@ sealed class SnippetJoiner(val id: String) {
     abstract fun constructContext(matches: List<EmbeddingMatch>): String
 }
 
+/** A basic joiner with content of each chunk in a separate match. */
 class BasicTemplateJoiner(_id: String) : SnippetJoiner(_id) {
     override fun constructContext(matches: List<EmbeddingMatch>) =
         AiPromptLibrary.lookupPrompt(id)
-            .fill("matches" to matches.mapIndexed { i, it ->
+            .fill(MATCHES_TEMPLATE to matches.mapIndexed { i, it ->
                 NameText(i + 1, it)
             })
 }
 
+/** A joiner with content of each chunk grouped by document, along with document metadata. */
 class GroupingTemplateJoiner(_id: String) : SnippetJoiner(_id) {
     override fun constructContext(matches: List<EmbeddingMatch>) =
-        matches.groupBy { it.shortDocName }.mapValues {
-            it.value.joinToString("\n...\n") { it.chunkText.trim() }
+        matches.groupBy { it.shortDocName }.entries.mapIndexed { i, en ->
+            val doc = en.value.first().document
+            val joinedInThisDoc = en.value.joinToString("\n...\n") { it.chunkText.trim() }.trim()
+            val docPrefix = doc.attributes[TEXT_DOC_ATTRIBUTE_CONTEXT_PREFIX] as? String ?: ""
+            NameText(i + 1, en.key, docPrefix, joinedInThisDoc)
         }.let {
-            AiPromptLibrary.lookupPrompt(id)
-                .fill("matches" to it.entries.mapIndexed { i, it -> NameText(i + 1, it.key, it.value) })
+            AiPromptLibrary.lookupPrompt(id).fill(MATCHES_TEMPLATE to it)
         }
 }
 
-private class NameText(val number: Int, val name: String, val text: String) {
-    constructor(index: Int, match: EmbeddingMatch) : this(index, match.document.browsable()!!.shortNameWithoutExtension, match.chunkText.trim())
+/** Utility class for holding the name and text of a match. This is used when filling in joiner templates with Mustache templates. */
+private class NameText(val number: Int, val name: String, val prefix: String, val text: String) {
+    constructor(index: Int, match: EmbeddingMatch) : this(index, match.document.browsable()!!.shortNameWithoutExtension, "", match.chunkText.trim())
 }
