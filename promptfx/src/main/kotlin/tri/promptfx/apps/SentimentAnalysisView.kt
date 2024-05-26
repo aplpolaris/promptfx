6552039ea2a04a1204826c62675ed20021f00a6d
Elisha Peterson <triathematician@gmail.com>
2024-05-26 14:30:58 -0400

    #57 sentiment view

--- promptfx/src/main/kotlin/tri/promptfx/apps/SentimentAnalysisView.kt
+++ promptfx/src/main/kotlin/tri/promptfx/apps/SentimentAnalysisView.kt
@@ -19,42 +19,12 @@
  */
 package tri.promptfx.apps
 
-import javafx.beans.property.SimpleStringProperty
-import tornadofx.combobox
-import tornadofx.field
-import tri.ai.openai.instructTextPlan
-import tri.promptfx.AiPlanTaskView
+import tri.promptfx.RuntimePromptView
 import tri.promptfx.RuntimePromptViewConfigs
-import tri.promptfx.ui.promptfield
 import tri.util.ui.NavigableWorkspaceViewImpl
 
 /** Plugin for the [SentimentAnalysisView]. */
 class SentimentAnalysisPlugin : NavigableWorkspaceViewImpl<SentimentAnalysisView>("Text", "Sentiment Analysis", SentimentAnalysisView::class)
 
 /** View designed to classify the sentiment of a text. */
-class SentimentAnalysisView: AiPlanTaskView("Sentiment Analysis",
-    "Enter text to determine sentiment (or provide a numbered list of items)") {
-
-    private val modeOptions = RuntimePromptViewConfigs.modes["sentiment"]!!.keys.toList()
-    private val sourceText = SimpleStringProperty("")
-    private val mode = SimpleStringProperty(modeOptions[0])
-
-    init {
-        addInputTextArea(sourceText)
-        parameters("Sentiment Mode") {
-            field("Mode") {
-                combobox(mode, modeOptions) { isEditable = true }
-            }
-            promptfield(promptId = "sentiment-classify", workspace = workspace)
-        }
-        addDefaultTextCompletionParameters(common)
-    }
-
-    override fun plan() = completionEngine.instructTextPlan("sentiment-classify",
-        instruct = mode.get(),
-        userText = sourceText.get(),
-        tokenLimit = common.maxTokens.value!!,
-        temp = common.temp.value,
-    )
-
-}
+class SentimentAnalysisView : RuntimePromptView(RuntimePromptViewConfigs.config("sentiment-analysis"))
