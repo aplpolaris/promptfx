c06587e6c74dc19d53abc29d9f758024450b6754
Elisha Peterson <triathematician@gmail.com>
2024-05-30 08:34:49 -0400

    #245 better ui in models view

--- promptfx/src/main/kotlin/tri/promptfx/PromptFxModels.kt
+++ promptfx/src/main/kotlin/tri/promptfx/PromptFxModels.kt
@@ -43,4 +43,11 @@ object PromptFxModels {
     fun visionLanguageModels() = policy.visionLanguageModels()
     fun visionLanguageModelDefault() = policy.visionLanguageModelDefault()
 
+    fun modelIds() =
+        textCompletionModels().map { it.modelId } +
+        embeddingModels().map { it.modelId } +
+        chatModels().map { it.modelId } +
+        imageModels().map { it.modelId } +
+        visionLanguageModels().map { it.modelId }
+
 }
--- promptfx/src/main/kotlin/tri/promptfx/api/ModelsView.kt
+++ promptfx/src/main/kotlin/tri/promptfx/api/ModelsView.kt
@@ -21,12 +21,14 @@ package tri.promptfx.api
 
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon
 import javafx.beans.property.SimpleObjectProperty
+import javafx.scene.control.ToggleGroup
 import javafx.scene.layout.Priority
 import tornadofx.*
 import tri.ai.core.ModelInfo
 import tri.ai.core.ModelType
 import tri.ai.core.TextPlugin
 import tri.promptfx.AiTaskView
+import tri.promptfx.PromptFxModels
 import tri.util.ui.graphic
 import java.util.function.Predicate
 
@@ -37,12 +39,22 @@ class ModelsView : AiTaskView("Models", "List all models from API call, sorted b
     private val filteredModels = sortedModels.filtered { true }
     private val selectedModel = SimpleObjectProperty<ModelInfo>()
 
+    private val modelFilter = SimpleObjectProperty(ModelInfoFilter.ALL).apply {
+        onChange { nue -> filteredModels.predicate = Predicate { nue!!.filter.invoke(it) } }
+    }
+    private val modelSort = SimpleObjectProperty(ModelInfoSort.ID_ASC).apply {
+        onChange { nue -> sortedModels.comparator = nue!!.comparator }
+    }
+
     init {
         hideParameters()
         outputPane.clear()
         runButton.isVisible = false
         runButton.isManaged = false
     }
+
+    private val sortToggle = ToggleGroup()
+    private val filterToggle = ToggleGroup()
 
     init {
         output {
@@ -55,51 +67,66 @@ class ModelsView : AiTaskView("Models", "List all models from API call, sorted b
                             action { refresh() }
                         }
                         menubutton("", FontAwesomeIcon.SORT_ALPHA_ASC.graphic) {
-                            item("Sort by Id") {
-                                action { sortedModels.comparator = compareBy { it.id } }
+                            radiomenuitem("Sort by Id", sortToggle) {
+                                isSelected = true
+                                action { modelSort.value = ModelInfoSort.ID_ASC }
+                                // listen to changes in modelSort because this can be selected by reset button
+                                modelSort.onChange {
+                                    if (it == ModelInfoSort.ID_ASC)
+                                        isSelected = true
+                                }
                             }
-                            item("Sort by Created") {
-                                action { sortedModels.comparator = compareBy { it.created } }
+                            radiomenuitem("Sort by Created", sortToggle) {
+                                action { modelSort.value = ModelInfoSort.CREATED_ASC }
                             }
-                            item("Sort by Source") {
-                                action { sortedModels.comparator = compareBy { it.source } }
+                            radiomenuitem("Sort by Source", sortToggle) {
+                                action { modelSort.value = ModelInfoSort.SOURCE_ASC }
                             }
-                            item("Sort by Type") {
-                                action { sortedModels.comparator = compareBy { it.type } }
+                            radiomenuitem("Sort by Type", sortToggle) {
+                                action { modelSort.value = ModelInfoSort.TYPE_ASC }
                             }
                         }
                         menubutton("", FontAwesomeIcon.SORT_ALPHA_DESC.graphic) {
-                            item("Sort by Id (descending)") {
-                                action { sortedModels.comparator = compareByDescending { it.id } }
+                            radiomenuitem("Sort by Id (descending)", sortToggle) {
+                                action { modelSort.value = ModelInfoSort.ID_DESC }
                             }
-                            item("Sort by Created (descending)") {
-                                action { sortedModels.comparator = compareByDescending { it.created } }
+                            radiomenuitem("Sort by Created (descending)", sortToggle) {
+                                action { modelSort.value = ModelInfoSort.CREATED_DESC }
                             }
-                            item("Sort by Source (descending)") {
-                                action { sortedModels.comparator = compareByDescending { it.source } }
+                            radiomenuitem("Sort by Source (descending)", sortToggle) {
+                                action { modelSort.value = ModelInfoSort.SOURCE_DESC }
                             }
-                            item("Sort by Type (descending)") {
-                                action { sortedModels.comparator = compareByDescending { it.type } }
+                            radiomenuitem("Sort by Type (descending)", sortToggle) {
+                                action { modelSort.value = ModelInfoSort.TYPE_DESC }
                             }
                         }
                         menubutton("", FontAwesomeIcon.FILTER.graphic) {
                             menu("Filter by Source") {
                                 TextPlugin.sources().forEach { source ->
-                                    item(source) {
-                                        action { filteredModels.predicate = Predicate { it.source == source } }
+                                    radiomenuitem(source, filterToggle) {
+                                        if (source == "Gemini")
+                                            graphic = FontAwesomeIcon.GOOGLE.graphic
+                                        action { modelFilter.value = ModelInfoFilter.ofSource(source) }
                                     }
                                 }
                             }
                             menu("Filter by Type") {
                                 ModelType.values().forEach { type ->
-                                    item(type.name) {
+                                    radiomenuitem(type.name, filterToggle) {
+                                        graphic = type.graphic()
                                         isMnemonicParsing = false
-                                        action { filteredModels.predicate = Predicate { it.type == type } }
+                                        action { modelFilter.value = ModelInfoFilter.ofType(type) }
                                     }
                                 }
                             }
-                            item("Clear Filter") {
-                                action { filteredModels.predicate = Predicate { true } }
+                            radiomenuitem("No Filter", filterToggle) {
+                                isSelected = true
+                                action { modelFilter.value = ModelInfoFilter.ALL }
+                                // listen to changes in modelFilter because this can be selected by reset button
+                                modelFilter.onChange {
+                                    if (it == ModelInfoFilter.ALL)
+                                        isSelected = true
+                                }
                             }
                         }
                     }
@@ -108,6 +135,12 @@ class ModelsView : AiTaskView("Models", "List all models from API call, sorted b
                         vgrow = Priority.ALWAYS
                         cellFormat {
                             text = "${it.id} (${it.source})"
+                            graphic = it.type.graphic()
+                            val inPolicy = it.id in PromptFxModels.modelIds()
+                            style = when {
+                                inPolicy -> ""
+                                else -> "-fx-font-style: italic"
+                            }
                         }
                         bindSelected(selectedModel)
                     }
@@ -174,9 +207,58 @@ class ModelsView : AiTaskView("Models", "List all models from API call, sorted b
             TextPlugin.modelInfo()
         } ui {
             models.setAll(it)
+            modelSort.value = ModelInfoSort.ID_ASC
+            modelFilter.value = ModelInfoFilter.ALL
         }
+    }
+
+    private fun ModelType.graphic() = when (this) {
+        ModelType.TEXT_COMPLETION -> FontAwesomeIcon.KEYBOARD_ALT.graphic
+        ModelType.TEXT_CHAT -> FontAwesomeIcon.COMMENTS.graphic
+        ModelType.TEXT_VISION_CHAT -> FontAwesomeIcon.IMAGE.graphic
+        ModelType.TEXT_EMBEDDING -> FontAwesomeIcon.FONT.graphic
+        ModelType.IMAGE_GENERATOR -> FontAwesomeIcon.CAMERA.graphic
+        ModelType.TEXT_TO_SPEECH -> FontAwesomeIcon.VOLUME_UP.graphic
+        ModelType.SPEECH_TO_TEXT -> FontAwesomeIcon.MICROPHONE.graphic
+        ModelType.MODERATION -> FontAwesomeIcon.EYE_SLASH.graphic
+        ModelType.QUESTION_ANSWER -> FontAwesomeIcon.QUESTION_CIRCLE.graphic
+        ModelType.UNKNOWN -> FontAwesomeIcon.CUBE.graphic
     }
 
     override suspend fun processUserInput() = TODO()
 
+}
+
+/** Sort options for models. */
+enum class ModelInfoSort(val comparator: Comparator<ModelInfo>) {
+    ID_ASC(compareBy { it.id }),
+    ID_DESC(compareByDescending { it.id }),
+    CREATED_ASC(compareBy { it.created }),
+    CREATED_DESC(compareByDescending { it.created }),
+    SOURCE_ASC(compareBy { it.source }),
+    SOURCE_DESC(compareByDescending { it.source }),
+    TYPE_ASC(compareBy { it.type }),
+    TYPE_DESC(compareByDescending { it.type });
+}
+
+/** Filter options for models. */
+enum class ModelInfoFilter(val filter: (ModelInfo) -> Boolean) {
+    SOURCE_OPENAI({ it.source == "OpenAI" }),
+    SOURCE_GEMINI({ it.source == "Gemini" }),
+    TYPE_TEXT_COMPLETION({ it.type == ModelType.TEXT_COMPLETION }),
+    TYPE_TEXT_CHAT({ it.type == ModelType.TEXT_CHAT }),
+    TYPE_TEXT_VISION_CHAT({ it.type == ModelType.TEXT_VISION_CHAT }),
+    TYPE_TEXT_EMBEDDING({ it.type == ModelType.TEXT_EMBEDDING }),
+    TYPE_IMAGE_GENERATOR({ it.type == ModelType.IMAGE_GENERATOR }),
+    TYPE_TEXT_TO_SPEECH({ it.type == ModelType.TEXT_TO_SPEECH }),
+    TYPE_SPEECH_TO_TEXT({ it.type == ModelType.SPEECH_TO_TEXT }),
+    TYPE_MODERATION({ it.type == ModelType.MODERATION }),
+    TYPE_QUESTION_ANSWER({ it.type == ModelType.QUESTION_ANSWER }),
+    TYPE_UNKNOWN({ it.type == ModelType.UNKNOWN }),
+    ALL({ true });
+
+    companion object {
+        fun ofType(type: ModelType) = valueOf("TYPE_${type.name}")
+        fun ofSource(source: String) = valueOf("SOURCE_${source.uppercase()}")
+    }
 }
