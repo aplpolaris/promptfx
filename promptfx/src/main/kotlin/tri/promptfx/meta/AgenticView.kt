91404e0bdc1caa8dbdc691e3d5007276adf5dbc6
Elisha Peterson <triathematician@gmail.com>
2025-08-01 20:22:19 -0400

    m

--- promptfx/src/main/kotlin/tri/promptfx/meta/AgenticView.kt
+++ promptfx/src/main/kotlin/tri/promptfx/meta/AgenticView.kt
@@ -34,7 +34,7 @@ import tri.util.ui.NavigableWorkspaceViewImpl
 import tri.util.ui.WorkspaceViewAffordance
 
 /** Plugin for the [AgenticView]. */
-class AgenticPlugin : NavigableWorkspaceViewImpl<AgenticView>("Meta", "Agentic Workflow", WorkspaceViewAffordance.Companion.INPUT_ONLY, AgenticView::class)
+class AgenticPlugin : NavigableWorkspaceViewImpl<AgenticView>("Meta", "Agentic Workflow", WorkspaceViewAffordance.INPUT_ONLY, AgenticView::class)
 
 /** View to execute an agentic workflow where the tools are view with text inputs and outputs. */
 class AgenticView : AiPlanTaskView("Agentic Workflow", "Describe a task and any necessary input to use multiple views within a common workflow.") {
